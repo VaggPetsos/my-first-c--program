642f84523c7ede6de341f704d2bc8d2425524a55
VaggPetsos <vaggcore@hotmail.com>
2025-11-02 17:43:27 +0200

    Create main.cs
    No comment!

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.generic;
+using.System.linq
+using System.Text
+using System.Threading.tasks;
+
+namespace CSharpTutorials
+{
+class Program
+{
+
+static void Main(string[] args)
+{
+
+string message = "Hello World!";
+
+Console.WriteLine(message);
+}
+}
+}
